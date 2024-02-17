276dedb90d51cb3cbd602adfa966a9ae7659ab3e
HARMAN <harm0053@algonquinlive.com>
2024-02-16 23:59:08 -0500

    Finished step 2

--- app/src/main/java/com/example/mylab2/MainActivity.java
+++ app/src/main/java/com/example/mylab2/MainActivity.java
@@ -10,5 +10,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView imgView;
+        Switch sw;
+        imgView = findViewById(R.id.flagview);
+        sw = findViewById(R.id.spin_switch);
+
+        sw.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(5000);
+                rotate.setRepeatCount(Animation.INFINITE);
+                rotate.setInterpolator(new LinearInterpolator());
+                imgView.startAnimation(rotate);
+            } else {
+                imgView.clearAnimation();
+            }
+        });
     }
 }
